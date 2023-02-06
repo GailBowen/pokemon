6702f7fe6d2569d78e78311765d181f0e450c7e9
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-02-06 18:58:55 +0000

    Get list of Pokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
   
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmandar"]);
+  const [pokemon, setPokemon] = useState([]);
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+    setPokemon(res.data.results.map(p => p.name))
+  });
   
 
 
